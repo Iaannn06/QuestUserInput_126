81d882f149443989cbb209a38582c0387d0b5c51
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-30 15:19:15 +0700

    menambahkan column dengan modifier

--- app/src/main/java/com/example/questuserinput_126/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_126/TeksEdit.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.mutableStateOf
 import android.R.string
-
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -18,6 +20,14 @@ fun FormDataDiri(modifier: Modifier
     var jenis by remember {mutableStateOf(value="")}
 
     val gender:list<string> = listOf("laki-laki","perempuan")
+
+    Column(modifier = Modifier.padding(top =50.dp),
+        verticalArrangement= Arrangement.Top,
+        HorizontalAlignment= Alignment.CenterHorizontally)
+
+        )
+
+
 
 
 }
