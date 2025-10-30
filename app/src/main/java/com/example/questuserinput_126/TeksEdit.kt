7f75941a0fb9223db013f4e2d6c9aba99574b06e
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-30 15:12:03 +0700

    menambahkan variabel-variabel untuk mengingat nilai masukan dari keyboard

--- app/src/main/java/com/example/questuserinput_126/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_126/TeksEdit.kt
@@ -1,10 +1,16 @@
 package com.example.questuserinput_126
 
 import androidx.compose.runtime.Composable
-Import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf?
+
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
+    var textNama by remember {mutableStateOf(value= "")}
+    var textAlamat by remember {mutableStateOf(value= "")}
+    var textJK by remember {mutableStateOf(value = "")}
+
 
 }
